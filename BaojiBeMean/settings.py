03916897fbe62388ba544ae2ea15fde3d6ad7d5c
Simran Ganju <ganjoosimran070@gmail.com>
2017-09-23 18:02:18 +0530

    why

--- BaojiBeMean/settings.py
+++ BaojiBeMean/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'Dard.apps.DardConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- Dard/models.py
+++ Dard/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Tatti(models.Model):
+    name = models.CharField(max_length=50)
+    amount = models.CharField(max_length=20)
+    kyu = models.CharField(max_length=30)
+    pfft = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.name+' '+self.amount
+
+class Susu(models.Model):
+    tatti = models.ForeignKey(Tatti, on_delete=models.CASCADE)
+    color = models.CharField(max_length=10)
+    aroma = models.CharField(max_length=20)
